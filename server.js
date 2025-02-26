400c208aad92b4a71b6dfc5ef5bc3add83467490
Rajankumar001 <chaudharyrajan387@gmail.com>
2025-02-26 01:48:31 -0800

    add vercel link

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app=express();
 const PORT=8000;
 const corsOptions = {
-    origin:  "http://localhost:3000",
+    origin:  "https://leader-board-fronted-47wt.vercel.app",
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     optionsSuccessStatus: 200, 
